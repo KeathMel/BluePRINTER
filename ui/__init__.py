b32711f2a5301720565077a418a0216f8a23873b
KeathMel <KeathiEmpr@protonmail.com>
2026-08-16 16:12:45 +0000

    Delete ui directory

--- ui/__init__.py
+++
@@ -1,5 +0,0 @@
-from .theme import apply_iron_man_theme
-from .viewer import ViewerWidget
-from .project_manager import ProjectManager, Project
-
-__all__ = ['apply_iron_man_theme', 'ViewerWidget', 'ProjectManager', 'Project']
